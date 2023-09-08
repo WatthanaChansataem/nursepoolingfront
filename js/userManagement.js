da1fed266bd8061d388c314af7d3b78a783297af
Rewadee <root>
2023-09-08 17:04:25 +0700

    Initial commit

--- js/userManagement.js
+++ js/userManagement.js
@@ -152,7 +152,7 @@ $(document).ready(function () {
 let SetupData = (function () {
   let loadHospital = function (defered) {
     $.ajax({
-      url: "https://localhost:7063/api/hospital/list",
+      url: "http://10.104.10.243:8082/api/hospital/list",
       type: "GET",
       headers: {
         Authorization: "Bearer " + localStorage.getItem("token"),
@@ -178,7 +178,7 @@ let SetupData = (function () {
 
   let loadLocation = function (defered) {
     $.ajax({
-      url: "https://localhost:7063/api/location/list",
+      url: "http://10.104.10.243:8082/api/location/list",
       type: "GET",
       headers: {
         Authorization: "Bearer " + localStorage.getItem("token"),
@@ -204,7 +204,7 @@ let SetupData = (function () {
 
   let loadDepartment = function (defered) {
     $.ajax({
-      url: "https://localhost:7063/api/department/list",
+      url: "http://10.104.10.243:8082/api/department/list",
       type: "GET",
       headers: {
         Authorization: "Bearer " + localStorage.getItem("token"),
@@ -230,7 +230,7 @@ let SetupData = (function () {
 
   let loadUserData = function (defered) {
     $.ajax({
-      url: "https://localhost:7063/api/user/details",
+      url: "http://10.104.10.243:8082/api/user/details",
       type: "GET",
       headers: {
         Authorization: "Bearer " + localStorage.getItem("token"),
@@ -241,7 +241,7 @@ let SetupData = (function () {
           $("#currentUserName").html(userData.firstName);
           $("#navProfileImg").attr(
             "src",
-            `https://localhost:7063/api/document/avatar/${userData.userId}`
+            `http://10.104.10.243:8082/api/document/avatar/${userData.userId}`
           );
           if (userData.role != userRoleConstant.Admin) {
             localStorage.clear();
@@ -266,7 +266,7 @@ let SetupData = (function () {
 
   let loadPosition = function (defered) {
     $.ajax({
-      url: "https://localhost:7063/api/position/list",
+      url: "http://10.104.10.243:8082/api/position/list",
       type: "GET",
       headers: {
         Authorization: "Bearer " + localStorage.getItem("token"),
@@ -559,7 +559,7 @@ let CreateDatatable = (function () {
         currentRow = $(this).closest("tr");
         $("#profileImg").attr(
           "src",
-          `https://localhost:7063/api/document/avatar/${data.userId}`
+          `http://10.104.10.243:8082/api/document/avatar/${data.userId}`
         );
 
         $("#firstName").html(
@@ -677,7 +677,7 @@ let CreateDatatable = (function () {
 
         Spinner.activateSpinner($("#editScheduleBtnModal"));
         $.ajax({
-          url: "https://localhost:7063/api/User/userManagement",
+          url: "http://10.104.10.243:8082/api/User/userManagement",
           type: "POST",
           headers: {
             Authorization: "Bearer " + localStorage.getItem("token"),
@@ -735,7 +735,7 @@ let LoadDutySchedule = function () {
     positionCode: parseInt($("#positionCode").val()),
   };
   $.ajax({
-    url: "https://localhost:7063/api/User/searchForUserManagement",
+    url: "http://10.104.10.243:8082/api/User/searchForUserManagement",
     type: "POST",
     headers: {
       Authorization: "Bearer " + localStorage.getItem("token"),
@@ -798,7 +798,7 @@ $("input[name=changeProfileImage]").on("change", function () {
 
 let upLoadFileWithContent = function (uploadFileData, defer) {
   $.ajax({
-    url: "https://localhost:7063/api/document/createWithContent",
+    url: "http://10.104.10.243:8082/api/document/createWithContent",
     method: "POST",
     data: uploadFileData,
     dataType: "json",
@@ -890,7 +890,7 @@ $("#addUserBtnModal").on("click", function () {
   }
 
   $.ajax({
-    url: "https://localhost:7063/api/User/createAdmin",
+    url: "http://10.104.10.243:8082/api/User/createAdmin",
     type: "POST",
     headers: {
       Authorization: "Bearer " + localStorage.getItem("token"),
