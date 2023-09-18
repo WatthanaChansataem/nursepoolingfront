e0b8a07f7bef398af633328c417bb77eb225b525
Rewadee <root>
2023-09-18 13:00:57 +0700

    Initial commit

--- js/_GlobalConfig.js
+++ js/_GlobalConfig.js
@@ -1,5 +1,7 @@
-let link = "http://10.104.10.243:8088";
-
+let link = "http://10.104.10.243:8082";
+//http://10.104.10.243:8088 internal
+//http://10.104.10.243:8082 uat
+//
 $(document).ready(function () {
   // console.log(link);
 });
