29afbe38ed90d9ee0e7c61ba941f324a06a0e8b5
Rewadee <root>
2023-09-27 14:18:32 +0700

    Initial commit

--- js/_GlobalConfig.js
+++ js/_GlobalConfig.js
@@ -1,5 +1,5 @@
-let link = "http://10.104.10.41:8060";
-//http://10.104.10.243:8082 prod
+let link = "http://10.104.10.243:8082";
+//http://10.104.10.41:8060 prod
 // "constring" : "Data Source=10.104.10.40;Initial Catalog=nursepooling;Persist Security Info=True;TrustServerCertificate=True;User ID=nursepooling;Password=wkJN19adw5vUxnYL; "
 //http://10.104.10.243:8082 uat
 //http://10.104.10.243:8088 internal
