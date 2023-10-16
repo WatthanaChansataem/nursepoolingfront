82beaebc08e8faf7ef87ccd893f2d07c731c8b3d
Rewadee <root>
2023-10-16 15:54:37 +0700

    push 1.1.1.0

--- js/_GlobalConfig.js
+++ js/_GlobalConfig.js
@@ -1,10 +1,5 @@
-let link = "https://localhost:7063";
-//http://10.104.10.41:8060 prod
-// "constring" : "Data Source=10.104.10.40;Initial Catalog=nursepooling;Persist Security Info=True;TrustServerCertificate=True;User ID=nursepooling;Password=wkJN19adw5vUxnYL; "
-//http://10.104.10.243:8082 uat
-//http://10.104.10.243:8088 internal
-//https://localhost:7063 local
+let link = "http://10.104.10.243:8082";
+let flink =
+  "http://nursepoolinginternal-nursepooling.apps.svh-openshift.bdms.co.th/";
 let maximumSize = 1024 * 1024 * 5;
-$(document).ready(function () {
-  // console.log(link);
-});
+$(document).ready(function () {});
