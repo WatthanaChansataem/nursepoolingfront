775c880e2c8f6ffc92224002baa946eb36ba7fd2
Rewadee <root>
2023-10-18 10:41:11 +0700

    fix bug phase 2

--- js/_GlobalConfig.js
+++ js/_GlobalConfig.js
@@ -1,4 +1,4 @@
 let link = "http://10.104.10.243:8082";
-let flink = "http://nursepoolinguat-nursepooling.apps.svh-openshift.bdms.co.th/";
+let flink = "http://nursepoolinguat-nursepooling.apps.svh-openshift.bdms.co.th";
 let maximumSize = 1024 * 1024 * 5;
 $(document).ready(function () {});
