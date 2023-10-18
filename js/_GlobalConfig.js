2111877ec143e40a00a9ec85cb15d07c4af22df1
Rewadee <root>
2023-10-18 10:40:56 +0700

    fix bug phase 2

--- js/_GlobalConfig.js
+++ js/_GlobalConfig.js
@@ -1,4 +1,4 @@
-let link = "http://10.104.10.243:8088";
-let flink = "http://nursepoolinginternal-nursepooling.apps.svh-openshift.bdms.co.th";
+let link = "http://10.104.10.243:8082";
+let flink = "http://nursepoolinguat-nursepooling.apps.svh-openshift.bdms.co.th/";
 let maximumSize = 1024 * 1024 * 5;
 $(document).ready(function () {});
