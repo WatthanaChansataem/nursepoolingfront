bea85cb14e2a36af461ce09963c1936906afc025
Rewadee <root>
2023-12-27 08:36:48 +0700

    change connection

--- js/_GlobalConfig.js
+++ js/_GlobalConfig.js
@@ -1,4 +1,5 @@
-let link = "https://nurse-pl-api.azurewebsites.net";
+let link =
+  "https://svnh-nursepooling-bdms-dev-api.apps.svh-ocp-test.bdms.co.th";
 let flink = "https://samitivej-nurse-pool.azurewebsites.net";
 let maximumSize = 1024 * 1024 * 5;
 $(document).ready(function () {});
